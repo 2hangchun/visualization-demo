267c1588f9a3778bdbf62e53528145ba5c0ecd40
2hangchun <18328019908@163.com>
2021-07-29 16:42:17 +0800

    v1.2

--- crane/main_page/index.js
+++ crane/main_page/index.js
@@ -8,34 +8,301 @@ module.exports = {
     components: [
         {
             component: 'div',
-            props:{
-                class:'container',
+            props: {
+                class: 'container',
                 $style: {
-                    width:'1920px',
-                    height:'1080px'
+                    width: '1920px',
+                    height: '1080px',
+                    backgroundColor: '#192342',
+                    display: 'flex',
+                    flexDirection: 'column'
                 }
             },
             children: [
                 {
                     component: 'div',
-                    content: 'hello world',
-                    props:{
-                        $style:{
-                            fontSize:'200px',
-                            color:'blue'
+                    props: {
+                        class: 'container__top',
+                        $style: {
+                            height: '78px',
+                            width: '1920px',
+                            $border: `'1px solid black'`,
+                            display: 'flex',
+                            justifyContent: 'center'
                         }
-                    }
+                    },
+                    children: [
+                        {
+                            component: 'div',
+                            content: '园区经济资源可视化',
+                            props: {
+                                $style: {
+                                    width: '500px',
+                                    height: '36px',
+                                    fontSize: '36px',
+                                    fontWeight: '600',
+                                    color: '#fff',
+                                    textAlign: 'center'
+                                }
+                            }
+                        },
+                    ]
                 },
                 {
                     component: 'div',
-                    content: 'hello js',
-                    props:{
-                        $style:{
-                            fontSize:'100px',
-                            color:'green'
+                    props: {
+                        class: 'container__bottom',
+                        $style: {
+                            width: '1820px',
+                            height: '1010px',
+                            $margin: `'63px 0 0 50px'`,
+                            display: 'flex',
+                            flexWrap: 'wrap'
                         }
-                    }
-                }
+                    },
+                    children: [
+                        {
+                            component: 'div',
+                            props: {
+                                class: 'container__bottom__left-top',
+                                $style: {
+                                    width: '764px',
+                                    height: '414px',
+                                    $border: `'1px solid red'`,
+                                    display: 'flex',
+                                    flexDirection: 'column'
+                                }
+                            },
+                            children: [
+                                {
+                                    component: 'div',
+                                    props: {
+                                        class: 'container__bottom__left-top__top',
+                                        $style: {
+                                            width: '720px',
+                                            height: '30px',
+                                            border: '1px solid green',
+                                            $margin: `'32px 0 0 22px'`
+                                        }
+                                    },
+                                    children: [
+                                        {
+                                            component: 'div',
+                                            content: 'xxx',
+                                            props: {
+                                                $style: {
+                                                    fontSize: '18px',
+                                                    color: '#0071fe',
+                                                    fontWeight: 500,
+                                                    letterSpacing: 0.5
+                                                }
+                                            }
+                                        },
+                                        {
+                                            component: 'img',
+                                            props: {
+                                                src: '',
+                                                alt: 'pic'
+                                            }
+                                        }
+                                    ]
+                                },
+                                {
+                                    component: 'div',
+                                    props: {
+                                        class: 'container__bottom__left-top__bottom',
+                                        $style: {
+                                            width: '713px',
+                                            height: '247px',
+                                            margin: '50px 0 0 23px',
+                                            $border: `'1px solid yellow'`
+                                        }
+                                    },
+                                }
+                            ]
+                        },
+                        {
+                            component: 'div',
+                            props: {
+                                class: 'container__bottom__right-top',
+                                $style: {
+                                    width: '1018px',
+                                    height: '414px',
+                                    marginLeft: '34px',
+                                    $border: `'1px solid red'`,
+                                    display: 'flex',
+                                    flexDirection: 'column'
+                                }
+                            },
+                            children: [
+                                {
+                                    component: 'div',
+                                    props: {
+                                        class: 'container__bottom__right-top__top',
+                                        $style: {
+                                            width: '976px',
+                                            height: '30px',
+                                            $border: `'1px solid green'`,
+                                        }
+                                    },
+                                    children: [
+                                        {
+                                            component: 'div',
+                                            content: 'xxx',
+                                            props: {
+                                                $style: {
+                                                    fontSize: '18px',
+                                                    color: '#0071fe',
+                                                    fontWeight: 500,
+                                                    letterSpacing: 0.5
+                                                }
+                                            }
+                                        },
+                                        {
+                                            component: 'img',
+                                            props: {
+                                                src: '',
+                                                alt: 'pic'
+                                            }
+                                        }
+                                    ]
+                                },
+                                {
+                                    component: 'div',
+                                    props: {
+                                        class: 'container__bottom__right-top__bottom',
+                                        $style: {
+                                            width: '992px',
+                                            height: '414px',
+                                            $border: `'1px solid yellow'`,
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            component: 'div',
+                            props: {
+                                class: 'container__bottom__left-bottom',
+                                $style: {
+                                    width: '1018px',
+                                    height: '414px',
+                                    marginTop: '-13px',
+                                    $border: `'1px solid red'`,
+                                    display: 'flex',
+                                    flexDirection: 'column'
+                                }
+                            },
+                            children: [
+                                {
+                                    component: 'div',
+                                    props: {
+                                        class: 'container__bottom__left-bottom__top',
+                                        $style: {
+                                            width: '976px',
+                                            height: '30px',
+                                            $border: `'1px solid green'`,
+                                        }
+                                    },
+                                    children: [
+                                        {
+                                            component: 'div',
+                                            content: 'xxx',
+                                            props: {
+                                                $style: {
+                                                    fontSize: '18px',
+                                                    color: '#0071fe',
+                                                    fontWeight: 500,
+                                                    letterSpacing: 0.5
+                                                }
+                                            }
+                                        },
+                                        {
+                                            component: 'img',
+                                            props: {
+                                                src: '',
+                                                alt: 'pic'
+                                            }
+                                        }
+                                    ]
+                                },
+                                {
+                                    component: 'div',
+                                    props: {
+                                        class: 'container__bottom__left-bottom__bottom',
+                                        $style: {
+                                            width: '966px',
+                                            height: '414px',
+                                            $border: `'1px solid yellow'`,
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            component: 'div',
+                            props: {
+                                class: 'container__bottom__right-bottom',
+                                $style: {
+                                    width: '764px',
+                                    height: '414px',
+                                    marginLeft: '34px',
+                                    marginTop: '-13px',
+                                    $border: `'1px solid red'`,
+                                    display: 'flex',
+                                    flexDirection: 'column'
+                                }
+                            },
+                            children: [
+                                {
+                                    component: 'div',
+                                    props: {
+                                        class: 'container__bottom__right-bottom__top',
+                                        $style: {
+                                            width: '722px',
+                                            height: '30px',
+                                            $border: `'1px solid green'`,
+                                        }
+                                    },
+                                    children: [
+                                        {
+                                            component: 'div',
+                                            content: 'xxx',
+                                            props: {
+                                                $style: {
+                                                    fontSize: '18px',
+                                                    color: '#0071fe',
+                                                    fontWeight: 500,
+                                                    letterSpacing: 0.5
+                                                }
+                                            }
+                                        },
+                                        {
+                                            component: 'img',
+                                            props: {
+                                                src: '',
+                                                alt: 'pic'
+                                            }
+                                        }
+                                    ]
+                                },
+                                {
+                                    component: 'div',
+                                    props: {
+                                        class: 'container__bottom__right-bottom__bottom',
+                                        $style: {
+                                            width: '760px',
+                                            height: '340px',
+                                            $border: `'1px solid yellow'`,
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+
+                    ]
+                },
+
             ]
         }
     ]
--- src/views/main_page.vue
+++ src/views/main_page.vue
@@ -1,11 +1,64 @@
 <template>
   <div class="main_page">
-    <div class="container" :style="{width: '1920px', height: '1080px'}">
-      <div :style="{fontSize: '200px', color: 'blue'}">
-        hello world
+    <div class="container"
+         :style="{width: '1920px', height: '1080px', backgroundColor: '#192342', display: 'flex', flexDirection: 'column'}">
+      <div class="container__top"
+           :style="{height: '78px', width: '1920px', border: '1px solid black', display: 'flex', justifyContent: 'center'}">
+        <div
+            :style="{width: '500px', height: '36px', fontSize: '36px', fontWeight: '600', color: '#fff', textAlign: 'center'}">
+          园区经济资源可视化
+        </div>
       </div>
-      <div :style="{fontSize: '100px', color: 'green'}">
-        hello js
+      <div class="container__bottom"
+           :style="{width: '1820px', height: '1010px', margin: '63px 0 0 50px', display: 'flex', flexWrap: 'wrap'}">
+        <div class="container__bottom__left-top"
+             :style="{width: '764px', height: '414px', border: '1px solid red', display: 'flex', flexDirection: 'column'}">
+          <div class="container__bottom__left-top__top"
+               :style="{width: '720px', height: '30px', border: '1px solid green',margin:'32px 0 0 22px'}">
+            <div :style="{fontSize: '18px', color: '#0071fe', fontWeight: '500', letterSpacing: '0.5'}">
+              xxx
+            </div>
+            <img src="" alt="pic"/>
+          </div>
+          <div class="container__bottom__left-top__bottom"
+               :style="{width: '713px', height: '247px', margin:'50px 0 0 23px',border: '1px solid yellow'}"/>
+        </div>
+        <div class="container__bottom__right-top"
+             :style="{width: '1018px', height: '414px', marginLeft: '34px', border: '1px solid red', display: 'flex', flexDirection: 'column'}">
+          <div class="container__bottom__right-top__top"
+               :style="{width: '976px', height: '30px', border: '1px solid green'}">
+            <div :style="{fontSize: '18px', color: '#0071fe', fontWeight: '500', letterSpacing: '0.5'}">
+              xxx
+            </div>
+            <img src="" alt="pic"/>
+          </div>
+          <div class="container__bottom__right-top__bottom"
+               :style="{width: '992px', height: '414px', border: '1px solid yellow'}"/>
+        </div>
+        <div class="container__bottom__left-bottom"
+             :style="{width: '1018px', height: '414px', marginTop: '-13px', border: '1px solid red', display: 'flex', flexDirection: 'column'}">
+          <div class="container__bottom__left-bottom__top"
+               :style="{width: '976px', height: '30px', border: '1px solid green'}">
+            <div :style="{fontSize: '18px', color: '#0071fe', fontWeight: '500', letterSpacing: '0.5'}">
+              xxx
+            </div>
+            <img src="" alt="pic"/>
+          </div>
+          <div class="container__bottom__left-bottom__bottom"
+               :style="{width: '966px', height: '414px', border: '1px solid yellow'}"/>
+        </div>
+        <div class="container__bottom__right-bottom"
+             :style="{width: '764px', height: '414px', marginLeft: '34px', marginTop: '-13px', border: '1px solid red', display: 'flex', flexDirection: 'column'}">
+          <div class="container__bottom__right-bottom__top"
+               :style="{width: '722px', height: '30px', border: '1px solid green'}">
+            <div :style="{fontSize: '18px', color: '#0071fe', fontWeight: '500', letterSpacing: '0.5'}">
+              xxx
+            </div>
+            <img src="" alt="pic"/>
+          </div>
+          <div class="container__bottom__right-bottom__bottom"
+               :style="{width: '760px', height: '340px', border: '1px solid yellow'}"/>
+        </div>
       </div>
     </div>
   </div>
