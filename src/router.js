e9b77cbb2306033604e0a142cf283eaf361cb6a3
2hangchun <18328019908@163.com>
2021-07-29 12:55:50 +0800

    Operational

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ export default new Router({
   base: '/',
   routes: [
     {
-      name: 'main',
+      name: 'main_page',
       path: '/',
       component: pages.main_page,
     },
---
+++ src/views/main_page.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main_page">
+    <div :style="{fontSize: '300px', color: 'red', fontWeight: '800'}">
+      hello world
+    </div>
+  </div>
+</template>
+
+<script>
+import BuiltInMixin from '../mixins/built_in'
+import 'echarts'
+import VChart from 'vue-echarts'
+export const main_page = {
+  mixins: [BuiltInMixin],
+
+  components: {
+    'v-chart':VChart
+  },
+
+  data () {
+    return {
+      craneStates: {
+      },
+    }
+  },
+}
+export default main_page
+</script>
